0918e98cce7b5c04e4c6c8f2194c46976c5ce179
Sedana Yoga <9g.yoga27@gmail.com>
2021-09-12 21:26:15 +0800

    Refactored as ES6 module

--- src/components/project-input.ts
+++ src/components/project-input.ts
@@ -1,83 +1,81 @@
-/// <reference path='base-component.ts'/>
-/// <reference path='../decorators/autobind.ts'/>
-/// <reference path='../util/validation.ts'/>
-/// <reference path='../state/project-state.ts'/>
+import { Component } from './base-component.js'
+import { Autobind } from '../decorators/autobind.js'
+import { Validatable, validate } from '../util/validation.js'
+import { projectState } from '../state/project-state.js'
 
-namespace App {
-  // ProjectInput Class
-  export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
-    titleInputElement: HTMLInputElement
-    descriptionInputElement: HTMLInputElement
-    peopleInputElement: HTMLInputElement
+// ProjectInput Class
+export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
+  titleInputElement: HTMLInputElement
+  descriptionInputElement: HTMLInputElement
+  peopleInputElement: HTMLInputElement
 
-    constructor() {
-      super('project-input', 'app', true, 'user-input')
+  constructor() {
+    super('project-input', 'app', true, 'user-input')
 
-      this.titleInputElement = this.element.querySelector(
-        '#title'
-      )! as HTMLInputElement
-      this.descriptionInputElement = this.element.querySelector(
-        '#description'
-      )! as HTMLInputElement
-      this.peopleInputElement = this.element.querySelector(
-        '#people'
-      )! as HTMLInputElement
-      this.configure()
-      this.renderContent()
+    this.titleInputElement = this.element.querySelector(
+      '#title'
+    )! as HTMLInputElement
+    this.descriptionInputElement = this.element.querySelector(
+      '#description'
+    )! as HTMLInputElement
+    this.peopleInputElement = this.element.querySelector(
+      '#people'
+    )! as HTMLInputElement
+    this.configure()
+    this.renderContent()
+  }
+
+  renderContent() {}
+
+  configure() {
+    this.element.addEventListener('submit', this.submitHandler)
+  }
+
+  private gatherUserInput(): [string, string, number] | void {
+    const enteredTitle = this.titleInputElement.value
+    const enteredDescription = this.descriptionInputElement.value
+    const enteredPeople = this.peopleInputElement.value
+    const titleValidatable: Validatable = {
+      value: enteredTitle,
+      required: true,
     }
-
-    renderContent() {}
-
-    configure() {
-      this.element.addEventListener('submit', this.submitHandler)
+    const descriptionValidatable: Validatable = {
+      value: enteredDescription,
+      required: true,
+      minLength: 5,
     }
-
-    private gatherUserInput(): [string, string, number] | void {
-      const enteredTitle = this.titleInputElement.value
-      const enteredDescription = this.descriptionInputElement.value
-      const enteredPeople = this.peopleInputElement.value
-      const titleValidatable: Validatable = {
-        value: enteredTitle,
-        required: true,
-      }
-      const descriptionValidatable: Validatable = {
-        value: enteredDescription,
-        required: true,
-        minLength: 5,
-      }
-      const peopleValidatable: Validatable = {
-        value: +enteredPeople,
-        required: true,
-        min: 1,
-        max: 5,
-      }
-      if (
-        !validate(titleValidatable) ||
-        !validate(descriptionValidatable) ||
-        !validate(peopleValidatable)
-      ) {
-        alert('Invalid Input Data, please try again!')
-        return
-      } else {
-        return [enteredTitle, enteredDescription, +enteredPeople]
-      }
+    const peopleValidatable: Validatable = {
+      value: +enteredPeople,
+      required: true,
+      min: 1,
+      max: 5,
     }
-
-    private clearInputs() {
-      this.titleInputElement.value = ''
-      this.descriptionInputElement.value = ''
-      this.peopleInputElement.value = ''
+    if (
+      !validate(titleValidatable) ||
+      !validate(descriptionValidatable) ||
+      !validate(peopleValidatable)
+    ) {
+      alert('Invalid Input Data, please try again!')
+      return
+    } else {
+      return [enteredTitle, enteredDescription, +enteredPeople]
     }
+  }
 
-    @Autobind
-    private submitHandler(event: Event) {
-      event.preventDefault()
-      const userInputs = this.gatherUserInput()
-      if (Array.isArray(userInputs)) {
-        const [title, description, people] = userInputs
-        projectState.addProject(title, description, people) // projectState is a global variable
-        this.clearInputs()
-      }
+  private clearInputs() {
+    this.titleInputElement.value = ''
+    this.descriptionInputElement.value = ''
+    this.peopleInputElement.value = ''
+  }
+
+  @Autobind
+  private submitHandler(event: Event) {
+    event.preventDefault()
+    const userInputs = this.gatherUserInput()
+    if (Array.isArray(userInputs)) {
+      const [title, description, people] = userInputs
+      projectState.addProject(title, description, people) // projectState is a global variable
+      this.clearInputs()
     }
   }
 }
--- src/state/project-state.ts
+++ src/state/project-state.ts
@@ -1,55 +1,54 @@
-namespace App {
-  // Project State Management
-  export type Listener<T> = (items: T[]) => void
+import { Project, ProjectStatus } from '../models/project.js'
+// Project State Management
+export type Listener<T> = (items: T[]) => void
 
-  export class State<T> {
-    protected listeners: Listener<T>[] = []
-    addListener(listenerFn: Listener<T>) {
-      this.listeners.push(listenerFn)
-    }
+export class State<T> {
+  protected listeners: Listener<T>[] = []
+  addListener(listenerFn: Listener<T>) {
+    this.listeners.push(listenerFn)
+  }
+}
+
+export class ProjectState extends State<Project> {
+  private projects: Project[] = []
+  private static instance: ProjectState
+
+  private constructor() {
+    super()
   }
 
-  export class ProjectState extends State<Project> {
-    private projects: Project[] = []
-    private static instance: ProjectState
-
-    private constructor() {
-      super()
-    }
-
-    static getInstance() {
-      if (this.instance) {
-        return this.instance
-      }
-      this.instance = new ProjectState()
+  static getInstance() {
+    if (this.instance) {
       return this.instance
     }
-
-    addProject(title: string, description: string, numOfPeople: number) {
-      const newProject = new Project(
-        Math.random().toString(),
-        title,
-        description,
-        numOfPeople,
-        ProjectStatus.Active // new project default status being Active
-      )
-      this.projects.push(newProject)
-      this.updateListeners()
-    }
-
-    moveProject(projectId: string, newStatus: ProjectStatus) {
-      const project = this.projects.find((prj) => prj.id === projectId)
-      if (project && project.status !== newStatus) {
-        project.status = newStatus
-        this.updateListeners()
-      }
-    }
-    private updateListeners() {
-      for (const listenerFn of this.listeners) {
-        listenerFn(this.projects.slice())
-      }
-    }
+    this.instance = new ProjectState()
+    return this.instance
   }
 
-  export const projectState = ProjectState.getInstance() // Singleton, calling only one static method of a class
+  addProject(title: string, description: string, numOfPeople: number) {
+    const newProject = new Project(
+      Math.random().toString(),
+      title,
+      description,
+      numOfPeople,
+      ProjectStatus.Active // new project default status being Active
+    )
+    this.projects.push(newProject)
+    this.updateListeners()
+  }
+
+  moveProject(projectId: string, newStatus: ProjectStatus) {
+    const project = this.projects.find((prj) => prj.id === projectId)
+    if (project && project.status !== newStatus) {
+      project.status = newStatus
+      this.updateListeners()
+    }
+  }
+  private updateListeners() {
+    for (const listenerFn of this.listeners) {
+      listenerFn(this.projects.slice())
+    }
+  }
 }
+
+export const projectState = ProjectState.getInstance() // Singleton, calling only one static method of a class
